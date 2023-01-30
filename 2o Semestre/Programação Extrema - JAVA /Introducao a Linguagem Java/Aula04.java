bad8d5c1aa9ec7217610e77b3af39fc7e0c0fdf4
PedroW21 <pedrow21br@gmail.com>
2023-01-30 08:57:26 -0300

    Fim do primeiro modulo

---
+++ 2o Semestre/Programação Extrema - JAVA /Introducao a Linguagem Java/Aula04.java
@@ -0,0 +1,10 @@
+public class Aula04 {
+    
+    public static void main(String[] args)
+    {
+        int idade = 18;
+        double peso = 85.9;
+        System.out.println("Tenho " + idade + " e peso " + peso);
+    }
+
+}
